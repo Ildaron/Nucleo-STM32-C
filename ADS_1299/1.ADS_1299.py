09002307c7615579cc7c6d9eb26ed78bc6428fac
Ildar <de.ikar@yandex.ru>
2020-05-08 22:57:57 +0900

    Update 1.ADS_1299.py

--- ADS_1299/1.ADS_1299.py
+++ ADS_1299/1.ADS_1299.py
@@ -1,161 +1,79 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
- 
-  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
 
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
 SPI_HandleTypeDef hspi3;
-
-TIM_HandleTypeDef htim1;
-
 UART_HandleTypeDef huart5;
 
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_SPI3_Init(void);
 static void MX_UART5_Init(void);
-static void MX_TIM1_Init(void);
-/* USER CODE BEGIN PFP */
+static void MX_SPI3_Init(void);
 
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
-  
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_SPI3_Init();
   MX_UART5_Init();
-  MX_TIM1_Init();
-  /* USER CODE BEGIN 2 */
-  HAL_TIM_Base_Start_IT(&htim1);
-  /* USER CODE END 2 */
+  MX_SPI3_Init();
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
   int received_byte;
   int SDATAC=17;
+
+
   while (1)
   {
 
-	  HAL_GPIO_WritePin(GPIOD, CS_Pin, GPIO_PIN_SET);
-	  void send_command(uint8_t cmd)
-
-	  {
-		    HAL_GPIO_WritePin(GPIOD, CS_Pin, GPIO_PIN_RESET);
-	        HAL_Delay(10);
-	        //SPI.transfer(cmd);
-	        HAL_SPI_Transmit(&hspi3, (uint16_t*)&cmd,1, 10);
-	        HAL_SPI_Receive(&hspi3, (uint16_t*)&received_byte, 1, 10);
-	        HAL_Delay(10);
-	        HAL_GPIO_WritePin(GPIOD, CS_Pin, GPIO_PIN_SET);
-	  }
 
 
+ 	  HAL_GPIO_WritePin(GPIOD, CS_Pin, GPIO_PIN_SET);
+ 	  void send_command(uint8_t cmd)
+ 	    {
+ 	  		    HAL_GPIO_WritePin(GPIOD, CS_Pin, GPIO_PIN_RESET);
+ 	  	        HAL_Delay(10);
+ 	  	        //SPI.transfer(cmd);
+ 	  	        HAL_SPI_Transmit(&hspi3, (uint16_t*)&cmd,1, 10);
+ 	  	        HAL_SPI_Receive(&hspi3, (uint16_t*)&received_byte, 1, 10);
+ 	  	        HAL_Delay(10);
+ 	  	        HAL_GPIO_WritePin(GPIOD, CS_Pin, GPIO_PIN_SET);
+ 	  	  }
 
 
-	  void send_data_by_uart (received_byte)
-	  {
-	  // step 1 - convert dataset
-	  char buffer[16];
-	  char buffer1[16];
-	  sprintf(buffer1, "%d\n", received_byte);
-      int a=0;
-	  for (a; a<strlen(buffer1); a=a+1) //
-	         {
-		   if (buffer1[a]!= 0)
-		   	        	      {
-		   buffer[0] = buffer1[a];
-		   HAL_UART_Transmit(&huart5, buffer, 1, 1000);
-	                          }
-	          }
 
-           a=0;
-        // step 2 - send by uart UART
-	       HAL_UART_Transmit(&huart5, "amigos", 6, 1000);  //"amigo\r\n\0"
 
-	  }
 
-	  int test=1234;
-	  send_data_by_uart (test);
+ 	  void send_data_by_uart (received_byte)
+ 	  	  {
+ 	  	  // step 1 - convert dataset
+ 	  	  char buffer[16];
+ 	  	  char buffer1[16];
+ 	  	  sprintf(buffer1, "%d\n", received_byte);
+ 	        int a=0;
+ 	  	  for (a; a<strlen(buffer1); a=a+1) //
+ 	  	         {
+ 	  		   if (buffer1[a]!= 0)
+ 	  		   	        	      {
+ 	  		   buffer[0] = buffer1[a];
+ 	  		   HAL_UART_Transmit(&huart5, buffer, 1, 1000);
+ 	  	                          }
+ 	  	          }
 
-	  HAL_Delay(500);
-	  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_10);
-	  HAL_Delay(500);
+ 	             a=0;
+ 	          // step 2 - send by uart UART
+ 	  	       HAL_UART_Transmit(&huart5, "amigos", 6, 1000);  //"amigo\r\n\0"
 
-  }
+ 	  	  }
+
+
+
+ 	      int test=1234;
+ 	  	  send_data_by_uart (test);
+ 	  	  HAL_Delay(1000);
+ 	  	  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_10);
+ 	  	  HAL_Delay(1000);
+   }
   /* USER CODE END 3 */
 }
 
@@ -236,52 +154,6 @@ static void MX_SPI3_Init(void)
 }
 
 /**
-  * @brief TIM1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_TIM1_Init(void)
-{
-
-  /* USER CODE BEGIN TIM1_Init 0 */
-
-  /* USER CODE END TIM1_Init 0 */
-
-  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-
-  /* USER CODE BEGIN TIM1_Init 1 */
-
-  /* USER CODE END TIM1_Init 1 */
-  htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 719;
-  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 49999;
-  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim1.Init.RepetitionCounter = 0;
-  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN TIM1_Init 2 */
-
-  /* USER CODE END TIM1_Init 2 */
-
-}
-
-/**
   * @brief UART5 Initialization Function
   * @param None
   * @retval None
@@ -297,7 +169,7 @@ static void MX_UART5_Init(void)
 
   /* USER CODE END UART5_Init 1 */
   huart5.Instance = UART5;
-  huart5.Init.BaudRate = 9600;
+  huart5.Init.BaudRate = 115200;
   huart5.Init.WordLength = UART_WORDLENGTH_8B;
   huart5.Init.StopBits = UART_STOPBITS_1;
   huart5.Init.Parity = UART_PARITY_NONE;
@@ -324,14 +196,14 @@ static void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOE_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOE_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOE, GPIO_PIN_15, GPIO_PIN_RESET);
@@ -340,16 +212,10 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10|GPIO_PIN_11, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOD, START_Pin|CS_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : PE4 */
-  GPIO_InitStruct.Pin = GPIO_PIN_4;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : PA6 */
-  GPIO_InitStruct.Pin = GPIO_PIN_6;
+  /*Configure GPIO pin : PA0 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -369,12 +235,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : CS_Pin */
-  GPIO_InitStruct.Pin = CS_Pin;
+  /*Configure GPIO pins : START_Pin CS_Pin */
+  GPIO_InitStruct.Pin = START_Pin|CS_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(CS_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
 }
 
