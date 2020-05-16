9b25ce44111f8c0b134d1953cd7b6f48c3ee70bf
Ildar <de.ikar@yandex.ru>
2020-05-16 22:39:21 +0900

    Update 1.Port_arduino.py

--- graph/1.Port_arduino.py
+++ graph/1.Port_arduino.py
@@ -1,3 +1,34 @@
+# pip install pyserial
+
+import serial                    # import pySerial module
+ComPort = serial.Serial('COM7') # open the COM Port
+ComPort.baudrate = 9600          # set Baud rate
+ComPort.bytesize = 8             # Number of data bits = 8
+ComPort.parity   = 'N'           # No parity
+ComPort.stopbits = 1             # Number of Stop bits = 1
+data = ComPort.readline()        # Wait and read data
+print(data)                      # print the received data
+ComPort.close()                  # Close the COM Port
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # initial source https://www.thepoorengineer.com/en/arduino-python-plot/
 # changed for windows
 from threading import Thread
