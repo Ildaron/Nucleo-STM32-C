0ee56c49139b15a4fc087cd18787f76fc5650b05
Ildar <de.ikar@yandex.ru>
2020-08-31 15:07:06 +0900

    Update 3.0_real_time.py

--- graph/3.0_real_time.py
+++ graph/3.0_real_time.py
@@ -3,7 +3,9 @@ import os
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+import functools
 import numpy as np
+import random as rd
 import matplotlib
 matplotlib.use("Qt5Agg")
 from matplotlib.figure import Figure
@@ -13,10 +15,21 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import time
 import threading
 import serial
+import datetime as dt
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
 
-ComPort = serial.Serial('COM13') 
-ComPort.baudrate = 9600          
+from scipy.signal import butter, lfilter, freqz
+import matplotlib.pyplot as plt
+from scipy import signal
+
+from scipy.signal import cheby1, freqz  # Чебышева 
+
+
+
+ComPort = serial.Serial('COM7') 
+ComPort.baudrate = 115200          
 ComPort.bytesize = 8            
 ComPort.parity   = 'N'           
 ComPort.stopbits = 1            
@@ -27,26 +40,23 @@ xs = []
 ys = []
 
 
-
 class CustomMainWindow(QMainWindow):
     def __init__(self):
         super(CustomMainWindow, self).__init__()
-        # the main window
+        # Define the geometry of the main window
         self.setGeometry(300, 300, 800, 400)
-        self.setWindowTitle("graph")
-
+        self.setWindowTitle("my first window")
+        # Create FRAME_A
         self.FRAME_A = QFrame(self)
         self.FRAME_A.setStyleSheet("QWidget { background-color: %s }" % QColor(210,210,235,255).name())
         self.LAYOUT_A = QGridLayout()
         self.FRAME_A.setLayout(self.LAYOUT_A)
         self.setCentralWidget(self.FRAME_A)
-        
-        #zoom button
-        self.zoomBtn = QPushButton(text = 'Scale')
-        self.zoomBtn.setFixedSize(100, 50)                
-        self.zoomBtn.clicked.connect(self.zoomBtnAction) #
-                
-        self.LAYOUT_A.addWidget(self.zoomBtn, *(0,0))        
+        # Place the zoom button
+        self.zoomBtn = QPushButton(text = 'zoom')
+        self.zoomBtn.setFixedSize(100, 50)
+        self.zoomBtn.clicked.connect(self.zoomBtnAction)
+        self.LAYOUT_A.addWidget(self.zoomBtn, *(0,0))
         # Place the matplotlib figure
         self.myFig = CustomFigCanvas()
         self.LAYOUT_A.addWidget(self.myFig, *(0,1))
@@ -58,8 +68,7 @@ class CustomMainWindow(QMainWindow):
 
     def zoomBtnAction(self):
         print("zoom in")
-        #self.FRAME_A = QFrame(self)
-        self.myFig.zoomIn(voli)
+        self.myFig.zoomIn(5)
         return
 
     def addData_callbackFunc(self, value):
@@ -67,14 +76,15 @@ class CustomMainWindow(QMainWindow):
         self.myFig.addData(value)
         return
 
+''' End Class '''
+
+
 class CustomFigCanvas(FigureCanvas, TimedAnimation):
     def __init__(self):
         self.addedData = []
         print(matplotlib.__version__)
-        # The data       
-        self.xlim = 2000 
-
-        
+        # The data
+        self.xlim = 1000
         self.n = np.linspace(0, self.xlim - 1, self.xlim)
         a = []
         b = []
@@ -85,7 +95,6 @@ class CustomFigCanvas(FigureCanvas, TimedAnimation):
         b.append(3.0)
         b.append(4.0)
         self.y = (self.n * 0.0) + 50
-        
         # The window
         self.fig = Figure(figsize=(5,5), dpi=100)
         self.ax1 = self.fig.add_subplot(111)
@@ -95,29 +104,54 @@ class CustomFigCanvas(FigureCanvas, TimedAnimation):
         self.line1 = Line2D([], [], color='blue')
         self.line1_tail = Line2D([], [], color='red', linewidth=2)
         self.line1_head = Line2D([], [], color='red', marker='o', markeredgecolor='r')
-        
         self.ax1.add_line(self.line1)
-        self.ax1.add_line(self.line1_tail)        
-        self.ax1.add_line(self.line1_head)            
-        self.ax1.set_xlim(0, self.xlim-1)
-        
+        self.ax1.add_line(self.line1_tail)
+        self.ax1.add_line(self.line1_head)
+        self.ax1.set_xlim(0, self.xlim - 1)
+        self.ax1.set_ylim(-1000, 2000) # ildar
         FigureCanvas.__init__(self, self.fig)
-        TimedAnimation.__init__(self, self.fig, interval = 1, blit = True)
+        TimedAnimation.__init__(self, self.fig, interval = 50, blit = True)
         return
 
     def new_frame_seq(self):
         return iter(range(self.n.size))
+    
+    def _init_draw(self):  # for quick operation
+        lines = [self.line1, self.line1_tail, self.line1_head]
+        for l in lines:
+            l.set_data([], [])
+        return
+
+
+    def _init_draw(self):
+        lines = [self.line1, self.line1_tail, self.line1_head]
+        for l in lines:
+            l.set_data([], [])
+        return
 
     def addData(self, value):
         self.addedData.append(value)
         return
-    def zoomIn(self, value):
-        a=100
-        self.ax1.set_ylim(value-10000,value+10000)
-        #self.ax1.set_xlim(-a-100,a+100)
-       # self.draw()  
-       # return
 
+    def zoomIn(self, value):
+        bottom = self.ax1.get_ylim()[0]
+        top = self.ax1.get_ylim()[1]
+        bottom += value
+        top -= value
+        self.ax1.set_ylim(bottom,top)
+        self.draw()
+        return
+
+    def _step(self, *args):
+        # Extends the _step() method for the TimedAnimation class.
+        try:
+            TimedAnimation._step(self, *args)
+        except Exception as e:
+            self.abc += 1
+            print(str(self.abc))
+            TimedAnimation._stop(self)
+            pass
+        return
 
     def _draw_frame(self, framedata):
         margin = 2
@@ -127,33 +161,51 @@ class CustomFigCanvas(FigureCanvas, TimedAnimation):
             del(self.addedData[0])
 
         self.line1.set_data(self.n[ 0 : self.n.size - margin ], self.y[ 0 : self.n.size - margin ])
-        #self.line1_tail.set_data(np.append(self.n[-10:-1 - margin], self.n[-1 - margin]), np.append(self.y[-10:-1 - margin], self.y[-1 - margin]))
-        #self.line1_head.set_data(self.n[-1 - margin], self.y[-1 - margin])
-     #   self._drawn_artists = [self.line1, self.line1_tail, self.line1_head]
+        self.line1_tail.set_data(np.append(self.n[-10:-1 - margin], self.n[-1 - margin]), np.append(self.y[-10:-1 - margin], self.y[-1 - margin]))
+        self.line1_head.set_data(self.n[-1 - margin], self.y[-1 - margin])
+        self._drawn_artists = [self.line1, self.line1_tail, self.line1_head]
         return
 
+''' End Class '''
+
+
+# You need to setup a signal slot mechanism, to
+# send data to your GUI in a thread-safe way.
+# Believe me, if you don't do this right, things
+# go very very wrong..
 class Communicate(QObject):
     data_signal = pyqtSignal(float)
 
+''' End Class '''
+
+
+
+
 def dataSendLoop(addData_callbackFunc):
+    # Setup the signal-slot mechanism.
     mySrc = Communicate()
     mySrc.data_signal.connect(addData_callbackFunc)
+
+    ar=0
+    data=np.array([])
+    check=11
     while(True):
+              
+
         voltage = ComPort.readline()
+        
         try:
          voltage=int(voltage)
-         print (voltage)        
+         
+            
         except ValueError:
-         voltage=0 
+         print("voltage=0 ")         
+         voltage=0
 
-        global voli
-        voli = voltage
-        ys.append(voli)        
-        mySrc.data_signal.emit(voli) # 
-        
-       
+        mySrc.data_signal.emit(voltage) 
+      
 if __name__== '__main__':
     app = QApplication(sys.argv)
     QApplication.setStyle(QStyleFactory.create('Plastique'))
     myGUI = CustomMainWindow()
-   # sys.exit(app.exec_())
+    sys.exit(app.exec_())
