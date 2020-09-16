db39d0cd73bb372a0b419bff7dfdac150a9b5c6f
Ildar <de.ikar@yandex.ru>
2020-09-17 00:43:31 +0900

    Update 1.Receive_data_socket.py

--- ESP8266/1.Receive_data_socket.py
+++ ESP8266/1.Receive_data_socket.py
@@ -2,7 +2,7 @@ import socket
 s = socket.socket(
 socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("ip_adress", port))
-
+#s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 while True:
  data = str(s.recv(1024),encoding='utf-8')
  print(data)
